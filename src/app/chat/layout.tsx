45fc0f478dcb13c96097cd0d4636f58262e3513c
Santhosh kumar <aigptkumar@gmail.com>
2025-08-14 04:56:24 +0000

    Perfect !
    Inside of this conversation window hide the footer section

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,4 +1,7 @@
 
+"use client";
+
+import { usePathname } from "next/navigation";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { BottomNav } from "@/components/bottom-nav";
 
@@ -7,11 +10,14 @@ export default function ChatLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isChatPage = pathname.includes('/chat/') && pathname.split('/').length > 2;
+
   return (
     <SidebarProvider>
       <div className="relative flex h-screen w-full flex-col">
         <div className="flex-1 overflow-y-auto">{children}</div>
-        <BottomNav />
+        {!isChatPage && <BottomNav />}
       </div>
     </SidebarProvider>
   )
