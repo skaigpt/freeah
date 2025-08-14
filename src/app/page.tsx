874bf991fdf85182e04d4e06cea65c08d22cce09
Santhosh kumar <aigptkumar@gmail.com>
2025-08-14 04:23:18 +0000

    No need to sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,12 +69,6 @@ export default function LoginPage() {
               Login
             </Button>
           </form>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="/register" className="underline">
-              Sign up
-            </Link>
-          </div>
         </CardContent>
       </Card>
     </div>
